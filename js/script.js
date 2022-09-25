52a661eb3e43657fe465140071f26e2d60694f3b
beqa <beqa.oqropiridze1994@gmail.com>
2022-09-25 12:57:32 +0400

    added burger menu function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
+// menu
+// burger-menus animacia js nawili
 var toggleButton = document.querySelector('.toggle-menu');
 var navBar = document.querySelector('.header-burgernav-bar');
 toggleButton.addEventListener('click', function () {
 	navBar.classList.toggle('toggle');
 }); 
+
+// burger-menu lists gaqroba gamochena
+var burgerlist = document.querySelector('.nav-list');
+
+toggleButton.addEventListener('click', () =>  {
+    if(burgerlist.style.display === 'none') {
+        burgerlist.style.display = 'block';
+    } else {
+        burgerlist.style.display = 'none';
+    }
+})
+// end menu
